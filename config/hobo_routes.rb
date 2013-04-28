0ec354ef1480c696a53dbf14e3ce342541a7cfd4
Gabriel Prat <jo@gabriel.prat.name>
2013-04-28 23:46:44 +0200

    require

--- config/hobo_routes.rb
+++ config/hobo_routes.rb
@@ -5,59 +5,6 @@
 Hoshinplan::Application.routes.draw do
 
 
-  # Resource routes for controller objectives
-  resources :objectives, :only => [:new, :edit, :show, :create, :update, :destroy]
-
-  # Owner routes for controller objectives
-  resources :areas, :as => :area, :only => [] do
-    resources :objectives, :only => [] do
-      get 'new', :on => :new, :action => 'new_for_area'
-      collection do
-        post 'create', :action => 'create_for_area'
-      end
-    end
-  end
-
-
-  # Resource routes for controller hoshins
-  resources :hoshins
-
-
-  # Resource routes for controller indicators
-  resources :indicators
-
-
-  # Resource routes for controller indicator_histories
-  resources :indicator_histories, :only => [:new, :edit, :show, :create, :update, :destroy]
-
-
-  # Resource routes for controller tasks
-  resources :tasks do
-    member do
-      put 'activate', :action => 'do_activate'
-      get 'activate'
-      put 'complete', :action => 'do_complete'
-      get 'complete'
-      put 'discard', :action => 'do_discard'
-      get 'discard'
-      put 'reactivate', :action => 'do_reactivate'
-      get 'reactivate'
-      put 'reactivate', :action => 'do_reactivate'
-      get 'reactivate'
-    end
-  end
-
-  # Owner routes for controller tasks
-  resources :objectives, :as => :objective, :only => [] do
-    resources :tasks, :only => [] do
-      get 'new', :on => :new, :action => 'new_for_objective'
-      collection do
-        post 'create', :action => 'create_for_objective'
-      end
-    end
-  end
-
-
   # Resource routes for controller areas
   resources :areas, :only => [:new, :edit, :show, :create, :update, :destroy]
 
@@ -91,5 +38,58 @@ Hoshinplan::Application.routes.draw do
   match 'login(.:format)' => 'users#login', :as => 'user_login'
   get 'logout(.:format)' => 'users#logout', :as => 'user_logout'
   match 'forgot_password(.:format)' => 'users#forgot_password', :as => 'user_forgot_password'
+
+
+  # Resource routes for controller tasks
+  resources :tasks do
+    member do
+      put 'activate', :action => 'do_activate'
+      get 'activate'
+      put 'complete', :action => 'do_complete'
+      get 'complete'
+      put 'discard', :action => 'do_discard'
+      get 'discard'
+      put 'reactivate', :action => 'do_reactivate'
+      get 'reactivate'
+      put 'reactivate', :action => 'do_reactivate'
+      get 'reactivate'
+    end
+  end
+
+  # Owner routes for controller tasks
+  resources :objectives, :as => :objective, :only => [] do
+    resources :tasks, :only => [] do
+      get 'new', :on => :new, :action => 'new_for_objective'
+      collection do
+        post 'create', :action => 'create_for_objective'
+      end
+    end
+  end
+
+
+  # Resource routes for controller indicators
+  resources :indicators
+
+
+  # Resource routes for controller objectives
+  resources :objectives, :only => [:new, :edit, :show, :create, :update, :destroy]
+
+  # Owner routes for controller objectives
+  resources :areas, :as => :area, :only => [] do
+    resources :objectives, :only => [] do
+      get 'new', :on => :new, :action => 'new_for_area'
+      collection do
+        post 'create', :action => 'create_for_area'
+      end
+    end
+  end
+
+
+  # Resource routes for controller indicator_histories
+  resources :indicator_histories, :only => [:new, :edit, :show, :create, :update, :destroy]
+
+
+  # Resource routes for controller hoshins
+  resources :hoshins
 
 end
