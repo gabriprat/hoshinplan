2656f876d6c4759ab41df5e8388256b165ae27a8
Gabriel Prat <jo@gabriel.prat.name>
2013-05-12 03:16:21 +0200

    restyle

--- config/hobo_routes.rb
+++ config/hobo_routes.rb
@@ -5,72 +5,8 @@
 Hoshinplan::Application.routes.draw do
 
 
-  # Resource routes for controller objectives
-  resources :objectives, :only => [:new, :edit, :show, :create, :update, :destroy] do
-    collection do
-      post 'reorder'
-    end
-  end
-
-  # Owner routes for controller objectives
-  resources :areas, :as => :area, :only => [] do
-    resources :objectives, :only => [] do
-      get 'new', :on => :new, :action => 'new_for_area'
-      collection do
-        post 'create', :action => 'create_for_area'
-      end
-    end
-  end
-
-
-  # Resource routes for controller indicators
-  resources :indicators, :only => [:new, :edit, :show, :create, :update, :destroy] do
-    collection do
-      post 'reorder'
-    end
-  end
-
-
-  # Resource routes for controller companies
-  resources :companies
-
-
-  # Resource routes for controller tasks
-  resources :tasks, :only => [:new, :edit, :show, :create, :update, :destroy] do
-    collection do
-      post 'reorder'
-    end
-    member do
-      put 'activate', :action => 'do_activate'
-      get 'activate'
-      put 'complete', :action => 'do_complete'
-      get 'complete'
-      put 'discard', :action => 'do_discard'
-      get 'discard'
-      put 'reactivate', :action => 'do_reactivate'
-      get 'reactivate'
-      put 'reactivate', :action => 'do_reactivate'
-      get 'reactivate'
-    end
-  end
-
-  # Owner routes for controller tasks
-  resources :objectives, :as => :objective, :only => [] do
-    resources :tasks, :only => [] do
-      get 'new', :on => :new, :action => 'new_for_objective'
-      collection do
-        post 'create', :action => 'create_for_objective'
-      end
-    end
-  end
-
-
-  # Resource routes for controller areas
-  resources :areas, :only => [:new, :edit, :show, :create, :update, :destroy] do
-    collection do
-      post 'reorder'
-    end
-  end
+  # Resource routes for controller indicator_histories
+  resources :indicator_histories, :only => [:new, :edit, :show, :create, :update, :destroy]
 
 
   # Resource routes for controller users
@@ -108,7 +44,71 @@ Hoshinplan::Application.routes.draw do
   end
 
 
-  # Resource routes for controller indicator_histories
-  resources :indicator_histories, :only => [:new, :edit, :show, :create, :update, :destroy]
+  # Resource routes for controller indicators
+  resources :indicators, :only => [:new, :edit, :show, :create, :update, :destroy] do
+    collection do
+      post 'reorder'
+    end
+  end
+
+
+  # Resource routes for controller companies
+  resources :companies
+
+
+  # Resource routes for controller areas
+  resources :areas, :only => [:new, :edit, :show, :create, :update, :destroy] do
+    collection do
+      post 'reorder'
+    end
+  end
+
+
+  # Resource routes for controller tasks
+  resources :tasks, :only => [:new, :edit, :show, :create, :update, :destroy] do
+    collection do
+      post 'reorder'
+    end
+    member do
+      put 'activate', :action => 'do_activate'
+      get 'activate'
+      put 'complete', :action => 'do_complete'
+      get 'complete'
+      put 'discard', :action => 'do_discard'
+      get 'discard'
+      put 'reactivate', :action => 'do_reactivate'
+      get 'reactivate'
+      put 'reactivate', :action => 'do_reactivate'
+      get 'reactivate'
+    end
+  end
+
+  # Owner routes for controller tasks
+  resources :objectives, :as => :objective, :only => [] do
+    resources :tasks, :only => [] do
+      get 'new', :on => :new, :action => 'new_for_objective'
+      collection do
+        post 'create', :action => 'create_for_objective'
+      end
+    end
+  end
+
+
+  # Resource routes for controller objectives
+  resources :objectives, :only => [:new, :edit, :show, :create, :update, :destroy] do
+    collection do
+      post 'reorder'
+    end
+  end
+
+  # Owner routes for controller objectives
+  resources :areas, :as => :area, :only => [] do
+    resources :objectives, :only => [] do
+      get 'new', :on => :new, :action => 'new_for_area'
+      collection do
+        post 'create', :action => 'create_for_area'
+      end
+    end
+  end
 
 end
