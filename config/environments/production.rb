fd296d797af16959a8e592f641bbbd7d269d1c5b
Gabriel Prat Masramon <gabriprat@MacBook-MacBook-Pro-de-Gabriel.local>
2013-10-20 01:32:48 +0200

    sendgrid config...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,6 +7,20 @@ Hoshinplan::Application.configure do
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+  
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+            :address        => 'smtp.sendgrid.net',
+            :port           => '587',
+            :authentication => :plain,
+            :user_name      => ENV['SENDGRID_USERNAME'],
+            :password       => ENV['SENDGRID_PASSWORD'],
+            :domain         => 'heroku.com'
+    }
+  config.action_mailer.asset_host = 'http://www.hoshinplan.com'
+  config.action_mailer.default_url_options = { host: 'http://www.hoshinplan.com', only_path: false }
+  
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
