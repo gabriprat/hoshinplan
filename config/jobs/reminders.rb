849a229a7bd3a14cf7b28b7792a7b2bf13ccf240
Gabriel Prat Masramon <gabriprat@MacBook-MacBook-Pro-de-Gabriel.local>
2014-01-17 01:23:35 +0100

    Reminders for tasks

--- config/jobs/reminders.rb
+++ config/jobs/reminders.rb
@@ -2,13 +2,24 @@ class SendUpdateReminders
   
   def perform
    puts "Initiating send remainders job!"
-    kpis = Indicator.unscoped.where("reminder = true and next_update < current_date").each { |kpi|
-      user = kpi.responsible
-      company = kpi.company
-      UserCompanyMailer.reminder(user, kpi, "KPI #{kpi.name} needs to be updated!", 
-      "You have to update the KPI #{kpi.name} for the hoshinplan " +
-       "of the company #{company.name}. To updated click the following link:").deliver
-    }
+   kpis = Indicator.unscoped.where("reminder = true and next_update < current_date").each { |kpi|
+     user = kpi.responsible
+     company = kpi.company
+     next if (user.nil? || company.nil?)
+     UserCompanyMailer.reminder(user, kpi, "KPI #{kpi.name} needs to be updated!", 
+     "You have to update the KPI #{kpi.name} for the hoshinplan " +
+      "of the company #{company.name}. To updated click the following link:").deliver
+   }
+   tasks = Task.unscoped.where("reminder = true and deadline < current_date and status = 'active'").each { |task|
+     user = task.responsible
+     company = task.company
+     next if (user.nil? || company.nil?)
+     UserCompanyMailer.reminder(user, task, "Task #{task.name} needs to be updated!", 
+     "You have to update the task #{task.name} for the hoshinplan " +
+      "of the company #{company.name}. To updated click the following link:").deliver
+   }
+    
+    
   end
   
   def say(text)
