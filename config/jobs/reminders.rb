5c9356c023ce9d8232eff36b4ae33568801ec183
Gabriel Prat Masramon <gabriprat@MacBook-MacBook-Pro-de-Gabriel.local>
2014-01-18 00:15:15 +0100

    send only 5 reminders

--- config/jobs/reminders.rb
+++ config/jobs/reminders.rb
@@ -2,8 +2,8 @@ class SendUpdateReminders
   
   def perform
    puts "Initiating send remainders job!"
-   kpis = User.joins('INNER JOIN "indicators" ON "indicators"."responsible_id" = "users"."id"').where("reminder = true and next_update < current_date")
-   tasks = User.joins('INNER JOIN "tasks" ON "tasks"."responsible_id" = "users"."id"').where("reminder = true and deadline < current_date and status = 'active'")
+   kpis = User.joins('INNER JOIN "indicators" ON "indicators"."responsible_id" = "users"."id"').where("reminder = true and next_update between current_date-5 and current_date")
+   tasks = User.joins('INNER JOIN "tasks" ON "tasks"."responsible_id" = "users"."id"').where("reminder = true and deadline between current_date-5 and current_date and status = 'active'")
    (kpis | tasks).each { |user|
      UserCompanyMailer.reminder(user, "You have KPIs or tasks to update!", 
      "You can access all the KPIs and tasks you have to update at your dashboard:").deliver
