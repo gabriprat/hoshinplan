55c485358800813e7a299645198905ff4b438758
Gabriel Prat Masramon <gabriprat@MacBook-MacBook-Pro-de-Gabriel.local>
2014-01-19 02:04:18 +0100

    Fixed: https://github.com/gabriprat/hoshinplan/issues/20

--- app/mailers/user_company_mailer.rb
+++ app/mailers/user_company_mailer.rb
@@ -1,11 +1,12 @@
 class UserCompanyMailer < ActionMailer::Base
   default :from => "gabri@hoshinplan.com"
 
-  def invite(user_company, subject, lead, message, callout, key)
+  def invite(user_company, subject, lead, message, callout, key, invitor)
     @key, @user, @user_company, @lead, @message, @callout = 
     key, user_company.user, user_company, lead, message, callout
     mail( :subject => subject,
-          :to      => @user.email_address )
+          :to      => @user.email_address,
+          :from    => invitor.name + " at hoshinplan.com <no-reply@hoshinplan.com>" )
   end
  
   def transition(user, subject, message)
