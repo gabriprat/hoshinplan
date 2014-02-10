0ffcdf3d1c8bd3d94b71bb95feac38300217b94c
Gabriel Prat Masramon <gabriprat@MacBook-MacBook-Pro-de-Gabriel.local>
2014-02-10 18:35:46 +0100

    sendreminders

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -4,8 +4,8 @@ class FrontController < ApplicationController
 
   # Require the user to be logged in for every other action on this controller
   # except :index. 
-  skip_before_filter :my_login_required, :only => :index
-
+  skip_before_filter :my_login_required, :only => [:index, :sendreminders]
+  
   def index
     if !current_user.nil? && !current_user.guest? && current_user.user_companies.empty?
       redirect_to "/first"
@@ -14,6 +14,17 @@ class FrontController < ApplicationController
  
   def first
     
+  end
+  
+  def sendreminders
+    puts "Initiating send remainders job!"
+    kpis = User.joins('INNER JOIN "indicators" ON "indicators"."responsible_id" = "users"."id"').where("reminder = true and next_update between current_date-5 and current_date")
+    tasks = User.joins('INNER JOIN "tasks" ON "tasks"."responsible_id" = "users"."id"').where("reminder = true and deadline between current_date-5 and current_date and status = 'active'")
+    (kpis | tasks).each { |user|
+      UserCompanyMailer.reminder(user, "You have KPIs or tasks to update!", 
+      "You can access all the KPIs and tasks you have to update at your dashboard:").deliver
+    }
+    render "index"
   end
 
   def summary
