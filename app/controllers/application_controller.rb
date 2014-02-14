a3cc6c2d87460f29fad5738f23d02de6cc9cdcb1
Gabriel Prat Masramon <gabriprat@MacBook-MacBook-Pro-de-Gabriel.local>
2014-02-14 02:51:52 +0100

    First Spanish localization, still needs revision.
    https://github.com/gabriprat/hoshinplan/issues/5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -70,8 +70,18 @@ class ApplicationController < ActionController::Base
   
   before_filter :set_locale
  
+ 
+  # Get locale code from request subdomain (like http://it.application.local:3000)
+  # You have to put something like:
+  #   127.0.0.1 gr.application.local
+  # in your /etc/hosts file to try this out locally
+  def extract_locale_from_subdomain
+    parsed_locale = request.subdomains.first
+    I18n.available_locales.include?(parsed_locale.to_sym) ? parsed_locale : nil if !parsed_locale.nil?
+  end
+  
   def set_locale
-    I18n.locale = params[:locale] || :en
+    I18n.locale = params[:locale] || extract_locale_from_subdomain || I18n.default_locale
   end
   
   
