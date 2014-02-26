2ea46def14eef6549148aa73f2690ea42f31461f
Gabriel Prat Masramon <gabriprat@MacBook-MacBook-Pro-de-Gabriel.local>
2014-02-26 21:27:10 +0100

    fixed-x long term goals and header

--- app/assets/javascripts/application/global.js
+++ app/assets/javascripts/application/global.js
@@ -68,7 +68,7 @@ var attachAutosubmit = function() {
 $(document).ready(attachAutosubmit);
 
 $(window).scroll(function () {  
-        $("body.fixed-headers .navbar, body.fixed-headers .content-header").map(function() {
+        $("body.fixed-headers .navbar, body.fixed-headers .content-header, .fixed-x").map(function() {
 		$(this).css({"width": $(this).width()});
 		$(this).css({"margin-left": $(window).scrollLeft()}); 
 	});
@@ -90,7 +90,7 @@ var colorize = function () {
 }
 
 $( window ).resize(function() {
-	$("body.fixed-headers .navbar, body.fixed-headers .content-header").map(function() {
+	$("body.fixed-headers .navbar, body.fixed-headers .content-header, .fixed-x").map(function() {
 		$(this).css({"width": "auto"});
 	});
   equalHeightSections();
