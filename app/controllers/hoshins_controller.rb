2ce29619edc6f15ad1523f8394d7cb9e86fc98cf
Gabriel Prat Masramon <gabriprat@gmail.com>
2014-03-23 00:09:28 +0100

    Fixed: https://github.com/gabriprat/hoshinplan/issues/62

--- app/controllers/hoshins_controller.rb
+++ app/controllers/hoshins_controller.rb
@@ -10,5 +10,14 @@ class HoshinsController < ApplicationController
   
   cache_sweeper :hoshins_sweeper
   
+  def create
+    if params["new-company-name"]
+      company = Company.new
+      company.name = params["new-company-name"]
+      company.save!
+      params["hoshin"]["company_id"] = company.id
+    end
+    hobo_create
+  end
 
 end
