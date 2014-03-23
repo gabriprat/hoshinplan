51cf688d91ddc689c0ddad2ff1c3a1827f1c6526
Gabriel Prat Masramon <gabriprat@gmail.com>
2014-03-23 14:13:08 +0100

    Slideshow improvements

--- backup_and_restore.sh
+++ backup_and_restore.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-heroku pgbackups:capture
+heroku pgbackups:capture --expire
 curl -o latest.dump `heroku pgbackups:url`
 pg_restore --verbose --clean --no-acl --no-owner -h localhost -d hoshinplan_dev latest.dump 
 rm latest.dump
