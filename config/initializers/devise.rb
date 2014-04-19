ea743c32069b105925547660720939f65683e93e
Gabriel Prat Masramon <gabriprat@gmail.com>
2014-04-19 03:08:36 +0200

    Fixing google auth problems... :(

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -5,6 +5,8 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :open_id, :name => 'openid'
   provider :google_oauth2, ENV["GOOGLE_CLIENT"], ENV["GOOGLE_SECRET"],
            {
-             :approval_prompt => "auto"
+             :approval_prompt => "auto",
+             :scope => "email profile"
+             
            }         
 end
