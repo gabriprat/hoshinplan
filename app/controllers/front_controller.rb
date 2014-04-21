0afdd783ca94cdf29e4bbe759cd522f5164e979d
Gabriel Prat Masramon <gabriprat@gmail.com>
2014-04-21 20:57:26 +0200

    reset counters job

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -4,7 +4,7 @@ class FrontController < ApplicationController
   
   # Require the user to be logged in for every other action on this controller
   # except :index. 
-  skip_before_filter :my_login_required, :only => [:index, :sendreminders, :updateindicators, :expirecaches]
+  skip_before_filter :my_login_required, :only => [:index, :sendreminders, :updateindicators, :expirecaches, :resetcounters]
   
   def index
     if !current_user.nil? && !current_user.guest? && current_user.user_companies.empty?
@@ -39,21 +39,25 @@ class FrontController < ApplicationController
       end
   end
   
+  def ll(text)
+    "#{DateTime.now.to_s} #{text}\n"
+  end
+  
   def sendreminders
-    @text = DateTime.now.to_s + " Initiating send reminders job!\n"
+    @text = ll "Initiating send reminders job!"
     kpis = User.at_hour(7).joins(:indicators).merge(Indicator.unscoped.due('5 day'))
     tasks = User.at_hour(7).joins(:tasks).merge(Task.unscoped.due('5 day'))
     (kpis | tasks).each { |user|
-      @text +=  DateTime.now.to_s + " User: " + user.email_address + "\n"
+      @text +=  ll " User: #{user.email_address}" 
       UserCompanyMailer.reminder(user, "You have KPIs or tasks to update!", 
       "You can access all the KPIs and tasks you have to update at your dashboard:").deliver
     }
-    @text += DateTime.now.to_s + " End send reminders job!"
+    @text += ll "End send reminders job!"
     render :text => @text, :content_type => Mime::TEXT
   end
   
   def updateindicators
-    @text = DateTime.now.to_s + " Initiating updateindicators job!\n"
+    @text = ll " Initiating updateindicators job!"
     ihs = IndicatorHistory.joins(:indicator => :responsible)
       .where("day = #{User::TODAY_SQL} 
         and (
@@ -66,43 +70,87 @@ class FrontController < ApplicationController
           )")
     ihs.each { |ih| 
       ind = ih.indicator
-      @text += DateTime.now.to_s + " " + ind.name + ": "
+      line += ind.name + ": "
       if (ind.goal.nil? || ind.goal != ih.goal)
-        @text += "goal #{ind.goal} => #{ih.goal}"
+        line += "goal #{ind.goal} => #{ih.goal}"
         ind.goal = ih.goal
       end
       if (ind.value.nil? || ind.value != ih.value)
-        @text += " value #{ind.value} => #{ih.value} last_update #{ind.last_update} => #{ih.day}"
+        line += " value #{ind.value} => #{ih.value} last_update #{ind.last_update} => #{ih.day}"
         ind.value = ih.value
       end
       if (ind.last_update.nil? || ind.last_update < ih.day) 
-        @text += " last_update #{ind.last_update} => #{ih.day}"
+        line += " last_update #{ind.last_update} => #{ih.day}"
         ind.last_update = ih.day
       end
-      @text += "\n"
+      @text += ll line
       ind.save!
     }
-    @text += DateTime.now.to_s + " End update indicators job!"
+    @text += ll "End update indicators job!"
     render :text => @text, :content_type => Mime::TEXT
   end
   
   def expirecaches
-    @text = DateTime.now.to_s + " Initiating expirecaches job!\n"
+    @text = ll "Initiating expirecaches job!"
     if Rails.configuration.action_controller.perform_caching
       kpis = Indicator.unscoped.due('0 day').merge(User.at_hour(0))
       kpis.each { |indicator| 
-        @text +=  DateTime.now.to_s + " KPI: " + indicator.name + "\n"
+        @text +=  ll " KPI: #{indicator.name}"
         expire_swept_caches_for(indicator)
         expire_swept_caches_for(indicator.area)
       }
       tasks = Task.unscoped.due_today.merge(User.at_hour(0))
       tasks.each { |task| 
-        @text +=  DateTime.now.to_s + " Task: " + task.name + "\n"
+        @text +=  ll "Task: #{task.name}"
         expire_swept_caches_for(task)
         expire_swept_caches_for(indicator.area)
       }
     end
-    @text += DateTime.now.to_s + " End expirecaches job!"
+    @text += ll "End expirecaches job!"
+    render :text => @text, :content_type => Mime::TEXT
+  end
+  
+  def exec_sqls(sqls)
+    lines = sqls.lines
+    lines.reject! {|line|
+      line.strip!
+      line.empty?
+    }
+    n = lines.count
+    ret = ll "Executing #{n} sqls..."
+    lines.each.with_index do |sql, i|
+      ret += ll "(#{i}/#{n}) Executing: #{sql}"
+      res = ActiveRecord::Base.connection.execute(sql)
+      ret += ll "====  Done! #{res.ntuples} rows affected"
+    end
+    ret
+  end
+  
+  def resetcounters
+    h = Hoshin.arel_table
+    
+    @text = exec_sqls("
+      update hoshins set goals_count = (select count(*) from goals where hoshin_id = hoshins.id) where goals_count != (select count(*) from goals where hoshin_id = hoshins.id);
+
+      update hoshins set areas_count = (select count(*) from areas where hoshin_id = hoshins.id) where areas_count != (select count(*) from areas where hoshin_id = hoshins.id);
+
+      update objectives set hoshin_id = (select hoshin_id from areas where areas.id = area_id) where hoshin_id != (select hoshin_id from areas where areas.id = area_id);
+      update hoshins set objectives_count = (select count(*) from objectives where hoshin_id = hoshins.id) where objectives_count != (select count(*) from objectives where hoshin_id = hoshins.id);
+
+      update indicators set hoshin_id = (select hoshin_id from objectives where objectives.id = objective_id) where hoshin_id != (select hoshin_id from objectives where objectives.id = objective_id);
+      update hoshins set indicators_count = (select count(*) from indicators where hoshin_id = hoshins.id) where indicators_count != (select count(*) from indicators where hoshin_id = hoshins.id);
+
+      update tasks set hoshin_id = (select hoshin_id from objectives where objectives.id = objective_id) where hoshin_id != (select hoshin_id from objectives where objectives.id = objective_id);
+      update hoshins set tasks_count = (select count(*) from tasks where hoshin_id = hoshins.id) where tasks_count != (select count(*) from tasks where hoshin_id = hoshins.id);
+
+      update areas set objectives_count = (select count(*) from objectives where area_id = areas.id) where objectives_count != (select count(*) from objectives where area_id = areas.id);
+
+      update indicators set area_id = (select area_id from objectives where objectives.id = objective_id) where area_id != (select area_id from objectives where objectives.id = objective_id);
+      update areas set indicators_count = (select count(*) from indicators where area_id = areas.id) where indicators_count != (select count(*) from indicators where area_id = areas.id);
+
+      update tasks set area_id = (select area_id from objectives where objectives.id = objective_id) where area_id != (select area_id from objectives where objectives.id = objective_id) ;
+      update areas set tasks_count = (select count(*) from tasks where area_id = areas.id) where tasks_count != (select count(*) from tasks where area_id = areas.id);
+    ");
     render :text => @text, :content_type => Mime::TEXT
   end
 
