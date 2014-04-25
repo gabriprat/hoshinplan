d577ea168fdaeae9d9cd3980ca4d2ef33537b0bc
Gabriel Prat Masramon <gabriprat@gmail.com>
2014-04-25 13:01:07 +0200

    Tutorial styles

--- app/controllers/cms_controller.rb
+++ app/controllers/cms_controller.rb
@@ -6,13 +6,7 @@ class CmsController < ApplicationController
   include CmsHelper
 
   def show 
-    key = params["key"]
-    @this = cmsGet(key)
-    @key = key
+    @key = params["key"]
   end  
   
-  def expire
-    expire_action :action => :show
-    redirect_to :action => :show
-  end
 end
