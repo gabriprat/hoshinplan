bdb989ad8e0404542486fa623559fda60c4a04ec
Gabriel Prat Masramon <gabriprat@gmail.com>
2014-04-28 23:15:14 +0200

    Fix hobo plugin error

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -12,5 +12,5 @@
 //= require hobo_bootstrap
 //= require hobo_jquery_ui
 //= require hobo_bootstrap_ui
-//= require hobo_simple_color
+//= require_tree ./hobo_simple_color
 //= require_tree ./admin
