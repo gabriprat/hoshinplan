fe589254e37f5b294941c0a568992e59f046b95f
Gabriel Prat Masramon <gabriprat@gmail.com>
2014-05-04 04:41:13 +0200

    uglify

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,6 +27,8 @@ Hoshinplan::Application.configure do
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
+  config.assets.css_compressor = :yui
+  config.assets.js_compressor = :uglify
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
