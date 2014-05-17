2c116ce3b69b5f344debb1767b1125e09c9198a9
Gabriel Prat Masramon <gabriprat@gmail.com>
2014-05-17 17:01:37 +0200

    Delete private file

--- backup_and_restore.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-heroku pgbackups:capture --expire
-curl -o latest.dump `heroku pgbackups:url`
-pg_restore --verbose --clean --no-acl --no-owner -h localhost -d hoshinplan_dev latest.dump 
-rm latest.dump
