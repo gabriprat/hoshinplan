a704b2895525cfafd86e1da83bcf2c61d52b98b8
gabrielp <gabrielp@infojobs.net>
2014-10-19 13:43:34 +0200

    Assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,7 @@ Hoshinplan::Application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   config.action_controller.asset_host = "static.hoshinplan.com"
+  config.asset_host = "static.hoshinplan.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
