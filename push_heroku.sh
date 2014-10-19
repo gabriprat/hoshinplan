8d8bdf20dde3951d9c42d4633f7e710f2405275e
gabrielp <gabrielp@infojobs.net>
2014-10-19 19:33:40 +0200

    css

--- push_heroku.sh
+++ push_heroku.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 RAILS_ENV=production bundle exec rake assets:precompile
+RAILS_ENV=production bundle exec rake assets:clean
 git add public/assets
 git commit -m assets public/assets/ 
 git push heroku master
