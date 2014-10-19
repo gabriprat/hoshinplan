0e9445a26d89b7d99500108fb44f4ec8bcb4dcf2
gabrielp <gabrielp@infojobs.net>
2014-10-19 12:41:22 +0200

    Assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Hoshinplan::Application.configure do
   #config.hobo.stable_cache_store = :redis_store, 'redis://rediscloud:E2rOg7rZl9fIpgtp@pub-redis-18280.eu-west-1-1.2.ec2.garantiadata.com:18280'
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_controller.asset_host = "http://static.hoshinplan.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
