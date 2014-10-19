4c8c0409011e4cacd9e0fbe9dba15d4dfb9ee9b2
gabrielp <gabrielp@infojobs.net>
2014-10-19 15:15:49 +0200

    Assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Hoshinplan::Application.configure do
   config.assets.digest = true
 
   # Defaults to nil and saved in location specified by config.assets.prefix
-  config.assets.manifest = "/assets"
+  config.assets.manifest = "/public/assets"
 
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
