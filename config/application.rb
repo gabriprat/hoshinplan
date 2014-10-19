6e3e5039a558fe012b98789520efe016abf8d4fd
gabrielp <gabrielp@infojobs.net>
2014-10-19 16:01:39 +0200

    Assets

--- config/application.rb
+++ config/application.rb
@@ -10,10 +10,14 @@ Bundler.require(:default, Rails.env)
 
 module Hoshinplan
   class Application < Rails::Application
+    # Hobo: the admin subsite loads admin.css & admin.js
+    config.assets.precompile += %w(admin.css admin.js)
     # Hobo: Named routes have changed in Hobo 2.0.   Set to false to emit both the 2.0 and 1.3 names.
     config.hobo.dont_emit_deprecated_routes = true
     # Hobo: remove support for ERB templates
     config.hobo.dryml_only_templates = true
+    # Hobo: the front subsite loads front.css & front.js
+    config.assets.precompile += %w(pdf.css front.css front.js)
   
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
