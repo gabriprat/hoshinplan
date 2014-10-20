6a46bfcdc9f3f5c70498f48df71ddf4d3b67e46f
gabrielp <gabrielp@infojobs.net>
2014-10-20 09:42:56 +0200

    Share sessions across domains

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Hoshinplan::Application.config.session_store ActionDispatch::Session::CacheStore, :expire_after => 15.days
-
+Hoshinplan::Application.config.session_store :cookie_store, :key => '_hoshinplan_session', :domain => :all
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
