3afe00450c9397620ffc8195cc12e8503968bef1
gabrielp <gabrielp@infojobs.net>
2014-10-20 10:42:18 +0200

    Remove sweepers

--- app/controllers/areas_sweeper.rb
+++ app/controllers/areas_sweeper.rb
@@ -4,19 +4,19 @@ class AreasSweeper < ActionController::Caching::Sweeper
 
   def after_create(area)
     if Rails.configuration.action_controller.perform_caching
-      expire_swept_caches_for(area)
+      #expire_swept_caches_for(area)
     end
   end
 
   def after_update(area)
     if Rails.configuration.action_controller.perform_caching
-      expire_swept_caches_for(area)
+      #expire_swept_caches_for(area)
     end
   end
 
   def after_destroy(area)
     if Rails.configuration.action_controller.perform_caching
-      expire_swept_caches_for(area)
+      #expire_swept_caches_for(area)
     end
   end
 end
--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -110,14 +110,14 @@ class FrontController < ApplicationController
       kpis = Indicator.unscoped.due('0 day').merge(User.at_hour(0))
       kpis.each { |indicator| 
         @text +=  ll " KPI: #{indicator.name}"
-        expire_swept_caches_for(indicator)
-        expire_swept_caches_for(indicator.area)
+        #expire_swept_caches_for(indicator)
+        #expire_swept_caches_for(indicator.area)
       }
       tasks = Task.unscoped.due_today.merge(User.at_hour(0))
       tasks.each { |task| 
         @text +=  ll "Task: #{task.name}"
-        expire_swept_caches_for(task)
-        expire_swept_caches_for(task.area)
+        #expire_swept_caches_for(task)
+        #expire_swept_caches_for(task.area)
       }
     end
     @text += ll "End expirecaches job!"
