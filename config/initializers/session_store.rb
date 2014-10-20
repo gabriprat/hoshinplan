bc6496327303e0c7c9ca08910494b6521cefc62e
gabrielp <gabrielp@infojobs.net>
2014-10-20 10:07:55 +0200

    Different session store at development to avoid memcached dependence

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Hoshinplan::Application.config.session_store :cookie_store, :key => '_hoshinplan_session', :domain => :all
+#Defined for every environment to avoid memcached dependence at development
+#Hoshinplan::Application.config.session_store ActionDispatch::Session::CacheStore, :expire_after => 15.days, :domain => :all
+
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
