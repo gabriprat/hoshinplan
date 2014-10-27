47c93e7b05ad922ce811e2e861b34d457247c8ed
gabrielp <gabrielp@infojobs.net>
2014-10-28 00:01:50 +0100

    Caching again

--- app/controllers/indicator_histories_sweeper.rb
+++ app/controllers/indicator_histories_sweeper.rb
@@ -4,21 +4,21 @@ class IndicatorHistoriesSweeper < ActionController::Caching::Sweeper
 
   def after_create(ih)
     if Rails.configuration.action_controller.perform_caching
-      #expire_swept_caches_for(ih.indicator.area)
+      expire_swept_caches_for(ih.indicator.area)
     end
   end
 
   def after_update(ih)
     if Rails.configuration.action_controller.perform_caching
-      #expire_swept_caches_for(ih.indicator.area)
-      #expire_swept_caches_for(ih.indicator)
+      expire_swept_caches_for(ih.indicator.area)
+      expire_swept_caches_for(ih.indicator)
     end
   end
 
   def after_destroy(ih)
     if Rails.configuration.action_controller.perform_caching
-      #expire_swept_caches_for(ih.indicator)
-      #expire_swept_caches_for(ih.indicator.area)
+      expire_swept_caches_for(ih.indicator)
+      expire_swept_caches_for(ih.indicator.area)
     end
   end
 end
--- app/controllers/users_sweeper.rb
+++ app/controllers/users_sweeper.rb
@@ -6,19 +6,19 @@ class UsersSweeper < ActionController::Caching::Sweeper
 
   def after_create(user)
     if Rails.configuration.action_controller.perform_caching
-      #expire_swept_caches_for(user)
+      expire_swept_caches_for(user)
     end
   end
 
   def after_update(user)
     if Rails.configuration.action_controller.perform_caching
-      #expire_swept_caches_for(user)
+      expire_swept_caches_for(user)
     end
   end
 
   def after_destroy(user)
     if Rails.configuration.action_controller.perform_caching
-      #expire_swept_caches_for(user)
+      expire_swept_caches_for(user)
     end
   end
 end
