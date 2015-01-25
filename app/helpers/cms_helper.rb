aecdc5481b9a6371d40b27b9da5e9d9b86201f70
gabrielp <gabrielp@infojobs.net>
2015-01-25 13:11:43 +0100

    Fixing errors

--- app/helpers/cms_helper.rb
+++ app/helpers/cms_helper.rb
@@ -24,7 +24,7 @@ module CmsHelper
       logger.debug "==================calling cms: #{key}" 
       res = ""
       conn = Faraday.new 'http://doc.hoshinplan.com' do |con|
-          con.adapter :typhoeus
+          con.adapter :excon
       end
       resp = conn.get '/' + key
       resp.on_complete {
