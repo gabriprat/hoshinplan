eedd5742e2daed10696d32648d8e204d78f4633e
gabrielp <gabrielp@infojobs.net>
2015-02-02 17:13:34 +0100

    Fix Child tasks & KPIs

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -2,7 +2,7 @@ class AreasController < ApplicationController
 
   hobo_model_controller
 
-  auto_actions :all, :except => [:new]
+  auto_actions :all, :except => [:new, :index]
   
   show_action :charts
   
