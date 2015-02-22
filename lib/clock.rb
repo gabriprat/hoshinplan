e92c201da1299f5fc950036c4e6c41a5a5a5d41c
gabrielp <gabrielp@infojobs.net>
2015-02-22 22:17:01 +0100

    Clockwork

--- lib/clock.rb
+++ lib/clock.rb
@@ -3,7 +3,8 @@ require File.expand_path('../../config/environment', __FILE__)
 Dir['../../config/jobs/*.rb'].each {|file| require File.expand_path(file)}
 include Clockwork
 
-every(1.minute, 'update_indicators') { Delayed::Job.enqueue Jobs::UpdateIndicators.new }
+every(1.hour, 'update_indicators') { Delayed::Job.enqueue Jobs::UpdateIndicators.new }
+every(1.hour, 'send_reminders') { Delayed::Job.enqueue Jobs::SendReminders.new }
 
 
 #, :at => '08:00'
