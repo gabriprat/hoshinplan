87b177414d0de9c4e1ae4ff38fe6e91d7fe9fcf3
gabrielp <gabrielp@infojobs.net>
2015-02-23 14:58:33 +0100

    Increase sleep delay

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -1,6 +1,6 @@
 Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'dj.log'))
 Delayed::Worker.destroy_failed_jobs = false
-Delayed::Worker.sleep_delay = 1
+Delayed::Worker.sleep_delay = 60
 Delayed::Worker.max_attempts = 3
 Delayed::Worker.max_run_time = 5.minutes
 Delayed::Worker.read_ahead = 10
