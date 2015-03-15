e4b3192e5b2809383c9c55bcd9f3b3dc46b6aee4
gabrielp <gabrielp@infojobs.net>
2015-03-16 00:30:29 +0100

    Push heroku

--- push_heroku.sh
+++ push_heroku.sh
@@ -4,4 +4,5 @@ RAILS_ENV=production bundle exec rake assets:clean
 RAILS_ENV=production bundle exec rake app:precompile_error_templates
 git add public/assets
 git commit -m assets public/assets/ 
+git commit -m errors public/404.html public/422.html public/500.html
 git push heroku master
