0e99e20829f5f6dd51d9724c9098a3ec0e61b6de
gabrielp <gabrielp@infojobs.net>
2015-05-06 13:33:24 +0200

    Unsubscribe link working

--- app/mailers/user_company_mailer.rb
+++ app/mailers/user_company_mailer.rb
@@ -1,8 +1,9 @@
 class UserCompanyMailer < ActionMailer::Base
   include Rails.application.routes.url_helpers
   
-  default :from => "Hoshinplan Team <hello@hoshinplan.com>"
-  
+  default :from => "Hoshinplan Team <hello@hoshinplan.com>",
+          'X-SMTPAPI' => '{"filters": { "subscriptiontrack": { "settings": {"replace": "[unsubscribe_url]", "enable": 1} } } }'
+          
   def render_email(name, params)
     user = params[:user]
     pars = {:unsubscribe_url => unsubscribe_user_url(user, :host => get_host_port(user))}
