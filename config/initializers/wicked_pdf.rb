2905c06a0878ca707eb1c3c9fc652ae5cdbd5f85
gabrielp <gabrielp@infojobs.net>
2015-06-17 18:07:56 +0200

    PDF

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,8 +1,7 @@
 WickedPdf.config = {
   :page_size => 'A4',
   :print_media_type => true,
-  :orientation => 'Landscape',
-  :show_as_html => true
+  :orientation => 'Landscape'
 }
 
 WickedPdf.config[:exe_path] = '/usr/local/bin/wkhtmltopdf' unless Rails.env.production?
