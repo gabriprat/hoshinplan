de8fd52ae1f8756f99e513586e29c926a871b1c7
Gabriel Prat Masramon <gabriprat@gmail.com>
2015-08-02 23:48:28 +0200

    Test send reminders

--- lib/clock.rb
+++ lib/clock.rb
@@ -7,5 +7,5 @@ include Clockwork
 
 every(1.hour, 'update_indicators', :at => '**:00') { Resque.enqueue Jobs::UpdateIndicators }
 every(1.day, 'health_update', :at => '23:30') { Resque.enqueue Jobs::HealthUpdate }
-every(1.hour, 'send_reminders', :at => '**:40') { Resque.enqueue Jobs::SendReminders }
+every(1.hour, 'send_reminders', :at => '**:52') { Resque.enqueue Jobs::SendReminders }
 every(1.hour, 'expire_cahces', :at => '**:20') { Resque.enqueue Jobs::ExpireCaches }
