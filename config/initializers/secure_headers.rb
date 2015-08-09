a4182d296159852aea14c7ce6dc1cf8e555a591f
Gabriel Prat Masramon <gabriprat@gmail.com>
2015-08-09 14:15:10 +0200

    Security

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -6,13 +6,13 @@
   config.x_download_options = 'noopen'
   config.x_permitted_cross_domain_policies = 'none'
   config.csp = {
-    :default_src => "self static.hoshinplan.com",
+    :default_src => "self static.hoshinplan.com *.userreport.com",
     :enforce => false,
     :font_src => 'self fonts.gstatic.com static.hoshinplan.com staticdoc.hoshinplan.com',
     :frame_src => "self https:",
     :style_src => "self 'unsafe-inline' fonts.googleapis.com static.hoshinplan.com staticdoc.hoshinplan.com",
-    :script_src => "self nonce 'unsafe-eval' 'unsafe-inline' static.hoshinplan.com staticdoc.hoshinplan.com app.box.com cdn.userreport.com settingsbucket.s3.amazonaws.com www.google-analytics.com ajax.cloudflare.com",
-    :img_src => "self static.hoshinplan.com staticdoc.hoshinplan.com shield.sitelock.com cdn.mxpnl.com",
+    :script_src => "self nonce 'unsafe-eval' 'unsafe-inline' static.hoshinplan.com staticdoc.hoshinplan.com app.box.com *.userreport.com settingsbucket.s3.amazonaws.com www.google-analytics.com ajax.cloudflare.com",
+    :img_src => "self static.hoshinplan.com staticdoc.hoshinplan.com shield.sitelock.com cdn.mxpnl.com *.doubleclick.com",
     :report_uri => '/uri-directive'
   }
   config.hpkp = false
