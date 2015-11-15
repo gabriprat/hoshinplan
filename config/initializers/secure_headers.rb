6fea99b3dd380ac1d217d49f238f264ed3e94e96
Gabriel Prat Masramon <gabriprat@gmail.com>
2015-11-15 20:42:10 +0100

    Add google

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -9,10 +9,10 @@
     :default_src => "'self' static.hoshinplan.com *.userreport.com api.mixpanel.com",
     :enforce => true,
     :font_src => "'self' fonts.gstatic.com static.hoshinplan.com staticdoc.hoshinplan.com",
-    :frame_src => "'self'",
+    :frame_src => "'self' accounts.google.com docs.google.com *.userreport.com",
     :style_src => "'self' 'unsafe-inline' fonts.googleapis.com static.hoshinplan.com staticdoc.hoshinplan.com",
     :script_src => "'self' 'unsafe-eval' 'unsafe-inline' static.hoshinplan.com staticdoc.hoshinplan.com apis.google.com www.dropbox.com app.box.com *.userreport.com settingsbucket.s3.amazonaws.com www.google-analytics.com ajax.cloudflare.com cdn.mxpnl.com",
-    :img_src => "'self' doc.hoshinplan.com static.hoshinplan.com staticdoc.hoshinplan.com hoshinplan.s3-eu-west-1.amazonaws.com shield.sitelock.com cdn.mxpnl.com stats.g.doubleclick.net www.google-analytics.com",
+    :img_src => "'self' doc.hoshinplan.com static.hoshinplan.com staticdoc.hoshinplan.com hoshinplan.s3-eu-west-1.amazonaws.com shield.sitelock.com cdn.mxpnl.com stats.g.doubleclick.net www.google-analytics.com *.userreport.com sqs.us-east-1.amazonaws.com",
     :report_uri => "/uri_dir_reports"
   }
   config.hpkp = false
