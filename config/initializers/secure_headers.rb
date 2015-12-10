150515c8eddb5b28da6448144665f65d98d98f2e
Gabriel Prat Masramon <gabriprat@gmail.com>
2015-12-10 23:18:03 +0100

    Allow videos

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -10,7 +10,7 @@
     :enforce => true,
     :connect_src => "'self' static.hoshinplan.com *.userreport.com api.mixpanel.com sqs.us-east-1.amazonaws.com",
     :font_src => "'self' fonts.gstatic.com static.hoshinplan.com staticdoc.hoshinplan.com data:",
-    :frame_src => "'self' accounts.google.com docs.google.com *.userreport.com",
+    :frame_src => "'self' accounts.google.com docs.google.com *.userreport.com www.youtube.com",
     :style_src => "'self' 'unsafe-inline' fonts.googleapis.com static.hoshinplan.com staticdoc.hoshinplan.com",
     :script_src => "'self' 'unsafe-eval' 'unsafe-inline' static.hoshinplan.com staticdoc.hoshinplan.com apis.google.com www.dropbox.com app.box.com *.userreport.com settingsbucket.s3.amazonaws.com www.google-analytics.com ajax.cloudflare.com cdn.mxpnl.com",
     :img_src => "'self' doc.hoshinplan.com static.hoshinplan.com staticdoc.hoshinplan.com hoshinplan.s3-eu-west-1.amazonaws.com shield.sitelock.com cdn.mxpnl.com stats.g.doubleclick.net www.google-analytics.com *.userreport.com sqs.us-east-1.amazonaws.com sha1test.cloudflaressl.net sha2test.cloudflaressl.net",
