d6c128c51810c663f5e47e71286f5bc98d394caa
Gabriel Prat Masramon <gabriprat@gmail.com>
2016-01-04 23:40:24 +0000

    Fix indicators counter at hoshins

--- app/models/indicator.rb
+++ app/models/indicator.rb
@@ -43,7 +43,7 @@ class Indicator < ActiveRecord::Base
   belongs_to :company, :null => false
 
   belongs_to :objective, :inverse_of => :indicators, :null => false
-  belongs_to :hoshin, :inverse_of => :indicators, :counter_cache => true, :null => false, :touch => true, :unscoped => true
+  belongs_to :hoshin, :inverse_of => :indicators, :counter_cache => true, :null => false, :unscoped => true
   
   belongs_to :area, :inverse_of => :indicators, :null => false
   belongs_to :responsible, :class_name => "User", :inverse_of => :indicators
