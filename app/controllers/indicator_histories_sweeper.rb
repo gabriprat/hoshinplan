a7a8181c861f6db0df2d7a1dfaa2795157fbf99b
Gabriel Prat Masramon <gabriprat@gmail.com>
2016-01-25 08:58:00 +0100

    Avoid errors

--- app/controllers/indicator_histories_sweeper.rb
+++ app/controllers/indicator_histories_sweeper.rb
@@ -6,25 +6,25 @@ class IndicatorHistoriesSweeper < ActionController::Caching::Sweeper
 
   def after_create(ih)
     if Rails.configuration.action_controller.perform_caching
-      expire_swept_caches_for(ih.indicator.area)
+      ind = Indicator.unscoped.find(ih.indicator_id)
+      expire_swept_caches_for(ind.area) if ind && ind.area
     end
   end
 
   def after_update(ih)
     if Rails.configuration.action_controller.perform_caching
       ind = Indicator.unscoped.find(ih.indicator_id)
-      expire_swept_caches_for(ind.area)
-      expire_swept_caches_for(ind)
+      expire_swept_caches_for(ind.area) if ind && ind.area
+      expire_swept_caches_for(ind) if ind 
       expire_swept_caches_for(ind.objective.parent.area) if ind && ind.objective && ind.objective.parent  
-      
     end
   end
 
   def after_destroy(ih)
     if Rails.configuration.action_controller.perform_caching
       ind = Indicator.unscoped.find(ih.indicator_id)
-      expire_swept_caches_for(ind)
-      expire_swept_caches_for(ind.area)
+      expire_swept_caches_for(ind) if ind
+      expire_swept_caches_for(ind.area) if ind && ind.area
       expire_swept_caches_for(ind.objective.parent.area) if ind && ind.objective && ind.objective.parent  
     end
   end
