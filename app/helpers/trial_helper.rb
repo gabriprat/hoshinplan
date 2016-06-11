e387a38a01374617dd0b396dcd6eaad22be101d9
Gabriel Prat Masramon <gabriprat@gmail.com>
2016-06-11 19:03:23 +0200

    Unlimited companies

--- app/helpers/trial_helper.rb
+++ app/helpers/trial_helper.rb
@@ -2,6 +2,7 @@ module TrialHelper
   def upgrade_button_visible?
     #fail (Company.current_company.subscriptions_count == 0).to_yaml + controller.controller_name.to_yaml + controller.action_name
     Company.current_company &&
+        !Company.current_company.unlimited &&
         Company.current_company.subscriptions_count == 0 &&
         controller.controller_name != 'billing_details' &&
         controller.action_name != 'upgrade'
