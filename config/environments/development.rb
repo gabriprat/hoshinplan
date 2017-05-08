4578be99a2a8e9930fb145ca454816b649f4a091
Gabriel Prat Masramon <gabriprat@gmail.com>
2017-05-08 23:06:27 +0200

    Log errors

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,7 +19,7 @@ Hoshinplan::Application.configure do
   
 
   # Show full error reports and disable caching
-  config.consider_all_requests_local       = true 
+  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
   config.cache_store = :redis_store, { :expires_in => 8.day }
   
