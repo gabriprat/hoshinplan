804bc00e95c1bd09fef09e3a39c83428065fac48
Gabriel Prat Masramon <gabriprat@gmail.com>
2017-08-27 19:53:19 +0200

    Fix user administration search

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -22,11 +22,16 @@ class Admin::UsersController < Admin::AdminSiteController
   end
   
   def index
-    @users = Hobo.find_by_search(params[:search], [User])[User.name] if params[:search].present?
+    if params[:search].present?
+      @users = Hobo.find_by_search(params[:search], [User.unscoped])[User.name]
+      @users ||= User.where('1=0')
+    else
+      @users = User.unscoped.all
+    end
 
-    @users ||= User.all
-
-    @users = @users.order_by(parse_sort_param(:name, :email_address, :id, :created_at, :administrator, :timezone, :state, :email_address, :preferred_view, :language)).paginate(:page => params[:page], :per_page => 15).load
+    sort = parse_sort_param(:name, :email_address, :id, :created_at, :administrator, :timezone, :state, :email_address, :preferred_view, :language)
+    sort ||= '-id'
+    @users = @users.order_by(sort).paginate(:page => params[:page], :per_page => 15).load
     hobo_index
   end
   
