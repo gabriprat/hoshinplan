339f5992cb4abff291963d849a71cc3606ad84fa
Gabriel Prat Masramon <gabriprat@gmail.com>
2020-02-25 20:29:07 +0100

    Payment date

--- app/controllers/sage_one_controller.rb
+++ app/controllers/sage_one_controller.rb
@@ -189,7 +189,7 @@ class SageOneController < ApplicationController
                       payment_method_id: "CREDIT_DEBIT",
                       contact_id: response[:contact][:id],
                       bank_account_id: CUENTA_CORRIENTE_57200000,
-                      date: Time.now,
+                      date: Time.at(response[:paid_at]),
                       total_amount: response[:total_amount],
                       allocated_artefacts: [
                           {
