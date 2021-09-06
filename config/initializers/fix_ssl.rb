119fda8a2e3f825daf8e551425712ae6f53136f6
Gabriel Prat Masramon <gabriprat@gmail.com>
2021-09-06 13:29:19 +0200

    Try to fix startup

--- config/initializers/fix_ssl.rb
+++ config/initializers/fix_ssl.rb
@@ -6,8 +6,8 @@ module Net
     alias_method :original_use_ssl=, :use_ssl=
 
     def use_ssl=(flag)
-      self.ca_file = Rails.root.join('lib/cacert.pem').to_s
-      self.verify_mode = OpenSSL::SSL::VERIFY_PEER
+      # self.ca_file = Rails.root.join('lib/cacert.pem').to_s
+      # self.verify_mode = OpenSSL::SSL::VERIFY_PEER
       self.original_use_ssl = flag
     end
   end
