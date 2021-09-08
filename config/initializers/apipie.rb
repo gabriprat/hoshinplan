61539cf8a4d38ecdccc8c79a255c57c03281fe6e
Gabriel Prat Masramon <gabriprat@gmail.com>
2021-09-08 23:08:05 +0200

    Fix api docs

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -3,6 +3,8 @@ Apipie.configure do |config|
   config.api_base_url            = ""
   config.doc_base_url            = "/apidocs"
   config.app_info                = ""
+  config.translate               = false
+  config.default_locale          = nil
   # where is your API defined?
   config.api_controllers_matcher = "#{Rails.root}/app/controllers/**/*.rb"
 end
