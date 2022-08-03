a12157a20916ae6faca25f8531a114a4fd80f265
Gabriel Prat Masramon <gabriprat@gmail.com>
2022-08-03 10:38:40 +0200

    Log to stdout

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,7 +1,7 @@
 require 'resque/failure/newrelic'
 require 'resque/log_formatters/tagged_very_verbose_formatter'
 
-Resque.logger = Rails.logger
+Resque.logger = Logger.new(STDOUT)
 
 Resque.before_first_fork = Proc.new { 
   Resque.logger.formatter = Resque::TaggedVeryVerboseFormatter.new
